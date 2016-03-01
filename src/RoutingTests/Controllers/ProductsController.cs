3af699afdabfde58aa83d1449a78b027ee51f7db
kuzhelov <deckster@mail.ru>
2016-03-01 23:55:10 +0300

    add action method parameters for attribute routing

---
+++ src/RoutingTests/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace RoutingTests.Controllers
+{
+    [Route("[controller]")]
+    public class ProductsController : Controller
+    {
+        [Route("{id}")]
+        public string GetProductById(int id)
+        {
+            return $"Product with ID {id} has been requested";
+        }
+    }
+}
