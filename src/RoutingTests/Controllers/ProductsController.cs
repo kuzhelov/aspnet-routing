f1d00e1c16ff00ec08b5a538ca1d272b5166dca7
kuzhelov <deckster@mail.ru>
2016-03-02 00:53:15 +0300

    fix undesired behavior in case when value of invalid type is provided as action's argument

--- src/RoutingTests/Controllers/ProductsController.cs
+++ src/RoutingTests/Controllers/ProductsController.cs
@@ -4,11 +4,26 @@ namespace RoutingTests.Controllers
 {
     [Route("[controller]")]
     public class ProductsController : Controller
-    {
+    {   
+        // in case if 'id' is not provided by the URL, 
+        // request will end up with the 'Page Not Found' (404)
+
+        // when there is no restriction  
         [Route("{id}")]
-        public string GetProductById(int id)
+        public IActionResult GetProductById(
+            // in case of strong int typing, the id will be provided with the default int value
+            // on failed conversion of value to int - i.e. with 0 value,
+            // and it could lead to undesired effects
+
+            // in case of Nullable typing - it would be possible to check if correct value is provided or not
+            int? id)
         {
-            return $"Product with ID {id} has been requested";
+            if (id.HasValue)
+            {
+                return Content($"Product with ID {id} has been requested");
+            }
+
+            return HttpNotFound();
         }
     }
 }
