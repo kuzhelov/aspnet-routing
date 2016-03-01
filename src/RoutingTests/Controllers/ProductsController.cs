1b411d42faa5a00bfa618715474039cfbe2b62e6
kuzhelov <deckster@mail.ru>
2016-03-02 01:12:00 +0300

    introduce attribute routing default values for action arguments

--- src/RoutingTests/Controllers/ProductsController.cs
+++ src/RoutingTests/Controllers/ProductsController.cs
@@ -7,8 +7,10 @@ namespace RoutingTests.Controllers
     {   
         // in case if 'id' does not meet restrictions, 
         // request will end up with the 'Page Not Found' (404)
-        [Route("{id:int}")]
-        public IActionResult GetProductById(int id)
+
+        // optional 'id' value will be provided for omitted URL segment
+        [Route("{id:int?}")]
+        public IActionResult GetProductById(int id = 100)
         {
             return Content($"Product with ID {id} has been requested");
         }
