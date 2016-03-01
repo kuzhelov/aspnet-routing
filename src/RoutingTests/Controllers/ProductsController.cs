24505e8656058bc7fdae6505fa0056d3ba001f54
kuzhelov <deckster@mail.ru>
2016-03-02 01:16:33 +0300

    introduce concept of controller's prefix omitting on attribute routing

--- src/RoutingTests/Controllers/ProductsController.cs
+++ src/RoutingTests/Controllers/ProductsController.cs
@@ -5,14 +5,17 @@ namespace RoutingTests.Controllers
     [Route("[controller]")]
     public class ProductsController : Controller
     {   
-        // in case if 'id' does not meet restrictions, 
-        // request will end up with the 'Page Not Found' (404)
-
-        // optional 'id' value will be provided for omitted URL segment
         [Route("{id:int?}")]
         public IActionResult GetProductById(int id = 100)
         {
             return Content($"Product with ID {id} has been requested");
+        }
+
+        // use ~ for omitting controller's route prefix
+        [Route("~/my-products")]
+        public string ListMyProducts()
+        {
+            return "Here all my products are listed";
         }
     }
 }
